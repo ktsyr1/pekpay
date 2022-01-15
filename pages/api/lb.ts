e03a1d01d5119bfbeaec5d81735eff1e9ee6e4ca
ktsyr1 <ktsyr1@gmail.com>
2022-01-15 20:31:14 +0200

    up

--- pages/api/lb.ts
+++ pages/api/lb.ts
@@ -15,6 +15,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     if (!data) NEW = await lebanonprices()
     else NEW = data
     NEW['name'] = 'ليرة لبناني'
-NEW =[NEW]
+    NEW = [NEW]
     res.status(200).json(NEW)
 } 
