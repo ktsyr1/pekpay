b8c58af4acbd93b54f396f2224c7ea7235dfb72c
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2022-04-16 14:00:31 +0300

    a

--- pages/api/x.ts
+++ pages/api/x.ts
@@ -1,9 +1,33 @@
-import sp_today from 'res/apis/sp-today'
-import lebanonprices from 'res/apis/lebanonprices'
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { Currency } from 'res/mongoDB';
 
-type Data = { msg: any }
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) { 
-    res.status(200).json({msg: 'dane update'})
-} 
+import type { NextApiRequest, NextApiResponse } from 'next'
+import cloudscraper from 'cloudscraper'
+//  jsdom 
+import { JSDOM } from 'jsdom'
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+    // cloudscraper for pesktop 
+    let { p } = req.query
+    var options = {
+        method: 'GET',
+        url: `https://apkcombo.com/ar/${p}/download/apk`
+    };
+
+    cloudscraper(options)
+        .then((data: any) => {
+            // jsdom
+
+            const { document } = (new JSDOM(data)).window
+            let e: any = document.querySelector('ul.file-list li a') 
+            let link: any =e.href
+
+            res.status(200).json({ link })
+        }).catch((err: any) => {
+            res.status(500).json(err)
+        })
+
+    // const { data } = await cloudscraper.get(url )
+    // res.status(200).json({ data: 'ok' })s
+}
+
+
+//     res.status(200).json({ data: '' })
+// }  // end of handler
